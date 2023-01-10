dc97e96276641da2b8a93849bcb07043511465d8
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-01-09 18:59:10 -0800

    Added formatting to five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,8 @@ let API_KEY = "&appid=38a8e3005a4683dccd22d2f534217a4a";
 //Event Listeners
 _ui.addEventListener("click", (event) => {
     event.preventDefault;
+    //If a new city name is input, display weather stats and 5-day forecast
+    //Add search to search history and local storage
     if(event.target.id == "submit-button"){
         getWeather(_searchInput.value);
         pastSearchArray.push(_searchInput.value);
@@ -33,6 +35,7 @@ _ui.addEventListener("click", (event) => {
         localStorage.setItem("pastSearches", JSON.stringify(pastSearchArray));
         populateSearchHistory();
     }
+    //Search past city but don't add to search history since it already exists
     if(event.target.className == "past-city-search"){
         getWeather(event.target.textContent);
     }
@@ -48,6 +51,7 @@ if(localStorage.getItem("pastSearches") != ""){
     pastSearchArray = (JSON.parse(localStorage.getItem("pastSearches")));
 }
 
+//Displays weather and updated 5-day forecast
 function getWeather(city){
     fetch(weatherURL + city + API_KEY)
     .then(response => response.json())
@@ -57,6 +61,7 @@ function getWeather(city){
     })
 }
 
+//Displays weather details in main city window
 function displayWeather(data){
     let tempInF = ((data.main.temp - 273.15) * 9/5) + 32;
     let tempInC = (data.main.temp - 273.15);
@@ -67,32 +72,48 @@ function displayWeather(data){
     _cityHumidity.textContent = `${data.main.humidity}%`;
 }
 
+//Displays weather details in 5-day results window
 function populateFiveDay(data){
     fetch(fiveDayURL + `lat=${data.coord.lat}&lon=${data.coord.lon}&cnt=5` + API_KEY)
         .then(response => response.json())
         .then( (fiveData) => {
             for(let i = 0; i < fiveData.list.length; i++){
+                //Reset 5-day forecast cards and get all relevant data
                 _fiveDayCards[i].innerHTML = "";
                 let date = today.add(i + 1, "day").format("MM/DD/YYYY");
                 let weatherIcon = fiveData.list[i].weather[0].icon;
                 let temp = (((fiveData.list[i].main.temp - 273.15) * 9/5) + 32).toFixed(2);
                 let wind = (fiveData.list[i].wind.speed);
                 let humidity = fiveData.list[i].main.humidity;
-
-                console.log(date, weatherIcon, temp, wind, humidity);
-                console.log("---------------");
-
                 let htmlArray = [date, weatherIcon, temp, wind, humidity];
 
                 for(let j = 0; j < htmlArray.length; j++){
                     let newDiv = document.createElement("div");
-                    newDiv.textContent = htmlArray[j];
+                    switch(j){
+                        case(2):
+                            newDiv.textContent = `Temp: ${htmlArray[j]}`;
+                            break;
+
+                        case(3):
+                            newDiv.textContent = `Wind: ${htmlArray[j]}`;
+                            break;
+
+                        case(4):
+                            newDiv.textContent = `Humidity: ${htmlArray[j]}`;
+                            break;
+
+                        default:
+                            newDiv.textContent = htmlArray[j];
+                            break;
+                    }
+                    
                     _fiveDayCards[i].append(newDiv);
                 }
             }
         })
 }
 
+//Populate on-screen search history
 function populateSearchHistory(){
     _pastSearchesWindow.innerHTML = ""
     
@@ -103,3 +124,5 @@ function populateSearchHistory(){
         _pastSearchesWindow.append(newDiv);
     }
 }
+
+populateSearchHistory();
